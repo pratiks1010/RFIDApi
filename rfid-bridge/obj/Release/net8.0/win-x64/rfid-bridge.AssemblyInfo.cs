d00fc91ffc5e273aef8029b07808b5528842f171
Anant <anantsahu21456@gmail.com>
2026-07-22 17:53:35 +0530

    chore: update dependencies and binaries for RFIDApi; bump runtime version to 8.0.29 and include new mscordaccore DLL

--- rfid-bridge/obj/Release/net8.0/win-x64/rfid-bridge.AssemblyInfo.cs
+++ rfid-bridge/obj/Release/net8.0/win-x64/rfid-bridge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rfid-bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0d1cd21f37752a20798ac667228933289c4e260")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc726f9fa898349a724da5229aa3b487aa1118f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("rfid-bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rfid-bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
